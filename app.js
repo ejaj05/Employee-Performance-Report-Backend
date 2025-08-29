0c99798f5b5c1d09ffa66b913b33084a99ebae3a
ejaj05 <alamejaj288@gmail.com>
2025-08-30 02:04:26 +0530

    updated mongo URI

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ app.use(cors(
 ));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  tlsInsecure: false})
 .then(() => {
   console.log('MongoDB connected');
 }).catch(err => {
